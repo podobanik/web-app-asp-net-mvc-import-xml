e59389c4e748b546b9349225779fcc52762c4eed
root <green.grocer@mail.ru>
2021-11-28 23:25:47 +1000

    #init

--- WebAppAspNetMvcExportExcel/WebAppAspNetMvcExportExcel/Models/ViewModels/ImportOrders/ImportOrderRowLog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebAppAspNetMvcExportExcel.Models
-{
-    public class ImportOrderRowLog
-    {
-        public int Id { get; set; }
-        public string Message { get; set; }
-        public ImportOrderRowLogType Type { get; set; }
-    }
-}
